ba3aa2a334bd32e3bb5bf6afe337787f7f1aeb0f
Pedro Aguiar <pedro_deaguiar@yahoo.com.br>
2018-11-30 23:31:22 +0000

    Adding terraform reference variables file

---
+++ variables.tf
@@ -0,0 +1,6 @@
+variable "subscription_id"      {}
+variable "location"             {}
+variable "access_key"           {}
+variable "project_name"         {}
+variable "environment"          {}
+variable "vnet_cidr"            {}
